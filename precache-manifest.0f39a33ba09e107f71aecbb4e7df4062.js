2582dbaf5848fb4617dcfcf2e61497fb05c16fe4
Benyamine Benyaagoub <benyamine@outlook.es>
2022-01-14 13:34:25 +0100

    Updates

---
+++ precache-manifest.0f39a33ba09e107f71aecbb4e7df4062.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f002148678ee5159680260466dcf5ee7",
+    "url": "/facturama/index.html"
+  },
+  {
+    "revision": "70e3c9d12f52908e6fda",
+    "url": "/facturama/static/css/2.ad8924ca.chunk.css"
+  },
+  {
+    "revision": "25a983cdfdc310951a3a",
+    "url": "/facturama/static/css/main.08b59345.chunk.css"
+  },
+  {
+    "revision": "70e3c9d12f52908e6fda",
+    "url": "/facturama/static/js/2.c23e356d.chunk.js"
+  },
+  {
+    "revision": "dca1570a96914e553a304510b0bddef2",
+    "url": "/facturama/static/js/2.c23e356d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a983cdfdc310951a3a",
+    "url": "/facturama/static/js/main.c619da4a.chunk.js"
+  },
+  {
+    "revision": "92cc5a7abed7e13a897c",
+    "url": "/facturama/static/js/runtime-main.234c74f5.js"
+  },
+  {
+    "revision": "33102bbda11907a853034d046af9109b",
+    "url": "/facturama/static/media/calculadora.33102bbd.svg"
+  },
+  {
+    "revision": "c006811f10d14df47c6da1d01b499195",
+    "url": "/facturama/static/media/direct-download.c006811f.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/facturama/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/facturama/static/media/resize.d4da359f.svg"
+  }
+]);
